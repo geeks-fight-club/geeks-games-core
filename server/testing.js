dadf359e03f6d819e5baa162f3c689713a51ca2a
elyas ghasemi <egh1374@gmail.com>
2016-12-11 10:29:15 +0330

    add clock pulse module

--- server/testing.js
+++ server/testing.js
@@ -1,22 +1,9 @@
-// //
-// //
-// //
 //
-// // const game = require('core/game/init');
-// //
-// // game(function(err, g) {
-// //   console.log(g);
-// // });
 //
-// const game = require('core/game/add_user');
 //
-// game('7752030f-8ea0-4b1e-a6c4-886830e5a8c1', function(err, user) {
+
+// const cp = require('core/clock/create');
 //
-//   if (err) {
-//     // TODO emmit it
-//     throw err.err;
-//   }
-//
-//   // socket.on('set_user_uuid', {  });
-//
-// });
+// cp(500,function(){
+//   console.log(new Date());
+// })
